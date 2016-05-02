8baf2705064f28f91485bf9ca02ca31c70c4863e
alsryan <alsryan@rams.colostate.edu>
2016-05-02 12:26:08 -0600

    Moved awake.php to assets/php/awake.php to communicate up/down status.

--- assets/php/awake.php
+++ assets/php/awake.php
@@ -1,11 +1,11 @@
 <?php
 header('Content-Type: text/json'); 
-$status = "down";
+$status = "up"; 
 
 $finalStatus = json_encode($status);
 echo "{\"status\":$finalStatus}";
 
-// 	<!-- siteName : status-->
+// 	<!-- siteName : status -->
 // 	<!-- siteName: petName : petKind : breed : datePosted -->	
 
 // <!-- siteName : status : petName : petKind : breed : datePosted -->
